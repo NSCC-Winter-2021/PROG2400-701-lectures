fd60d91f8d59698c9f945dfe8e5c4398974b0983
Brian Shewan <brian.shewan@nscc.ca>
2021-01-19 12:06:28 -0400

    added stringstream example

--- CommandParsing/main.cpp
+++ CommandParsing/main.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
+#include <sstream>
+#include <cstring>
 
 using namespace std;
 
 int main() {
 
+    char input[256];
     char command[32];
     int start;
     int end;
 
     cout << "Enter a command: ";
-    cin >> command >> start >> end;
+    cin.getline(input, 256);
 
-    cout << "Command: " << command << endl;
-    cout << "Start: " << start << endl;
-    cout << "End: " << end << endl;
+    stringstream ss;
+    ss << input;
+
+    ss >> command;
+
+    if (strlen(command) > 1) {
+        cout << "You entered text." << endl;
+    } else {
+        cout << "You entered a " << command << " command" << endl;
+
+        ss >> start;
+        ss >> end;
+
+        cout << "Command: " << command << endl;
+        cout << "Start: " << start << endl;
+        cout << "End: " << end << endl;
+
+    }
 
     return 0;
 }
